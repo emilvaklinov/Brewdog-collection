9857ef143140d867e6a07bc905f2f5de73e5707c
trakieca <emil_vaklinov@yahoo.com>
2018-09-16 19:01:53 +0100

    new table created

--- src/app.js
+++ src/app.js
@@ -18,4 +18,38 @@ beerDropdown.bindEvents();
 const beerInfoContainer = document.querySelector('div#beer');
 const beerInfoView = new BeerInfoView(beerInfoContainer);
 beerInfoView.bindEvents();
+
+
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const newVisitorForm = document.querySelector('#new-visitor-form');
+  newVisitorForm.addEventListener('submit', handleNewVisitorFormSubmit);
+  // console.log('JavaScript loaded');
+  const button = document.querySelector('#delete');
+  button.addEventListener('click', handleDelleteButtonClick);
+
+// });
+const handleNewVisitorFormSubmit = function (event) {
+  event.preventDefault();
+  let paragraphResult = document.createElement('added');
+  const firstName = event.target.first_name.value
+  const lastName = event.target.last_name.value
+  const address = event.target.address.value
+  const beer = event.target.address.value
+  const quontity = event.target.quantity.value
+  const payment = event.target.payment.value
+  const date = event.target.date.value
+
+  // const time = document.querySelector('#txt').textContent;
+
+
+  paragraphResult.textContent = `Name: ${firstName} ${lastName} |
+  Address: ${address} | Beer: ${beer} | Quantity: ${quontity} | Payment: ${payment} | Date: ${date} | Time: ${time}; `
+  const added = document.querySelector('#added');
+  added.appendChild(paragraphResult);
+  document.querySelector('#new-visitor-form');
+
+}
 });
